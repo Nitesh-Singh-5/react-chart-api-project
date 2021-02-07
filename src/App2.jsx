d5dc0059efbf8b08094ee35c406ccad85d4334d3
nitesh5801 <67116285+nitesh5801@users.noreply.github.com>
2021-02-07 20:59:50 +0530

    Initialize project using Create React App

---
+++ src/App2.jsx
@@ -0,0 +1,80 @@
+import React, { useState, useEffect } from "react";
+import { Line } from "react-chartjs-2";
+import axios from "axios";
+import "./App.css";
+
+const Apps = () => {
+  const [chartData, setChartData] = useState({});
+  const [employeeSalary, setEmployeeSalary] = useState([]);
+  const [employeeAge, setEmployeeAge] = useState([]);
+
+  const chart = () => {
+    let empSal = [];
+    let empAge = [];
+    axios
+      .get("http://dummy.restapiexample.com/api/v1/employees")
+      .then((res) => {
+        console.log(res);
+        for (const dataObj of res.data.data) {
+          empSal.push(parseInt(dataObj.employee_salary));
+          empAge.push(parseInt(dataObj.employee_age));
+        }
+        setChartData({
+          labels: empAge,
+          datasets: [
+            {
+              label: "level of thiccness",
+              data: empSal,
+              backgroundColor: ["#F8F8F8"],
+              borderWidth: 4,
+            },
+          ],
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    console.log(empSal, empAge);
+  };
+
+  useEffect(() => {
+    chart();
+  }, []);
+  return (
+    <div className="App">
+      <h1>Data visualization using React</h1>
+      <div>
+        <Line
+          data={chartData}
+          options={{
+            responsive: true,
+            title: { text: "THICCNESS SCALE", display: true },
+            scales: {
+              yAxes: [
+                {
+                  ticks: {
+                    autoSkip: true,
+                    maxTicksLimit: 10,
+                    beginAtZero: true,
+                  },
+                  gridLines: {
+                    display: false,
+                  },
+                },
+              ],
+              xAxes: [
+                {
+                  gridLines: {
+                    display: false,
+                  },
+                },
+              ],
+            },
+          }}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Apps;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import Apps from './App2'
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(<Apps />, document.getElementById('root'));
